a9a49d236a04f5535e12de41452a71a89ff6c221
KshithijaS <69100816+KshithijaS@users.noreply.github.com>
2022-06-01 11:13:59 +0530

    edited StoreBackendApplication.java

--- store-backend/src/main/java/com/sprint2/storebackend/StoreBackendApplication.java
+++ store-backend/src/main/java/com/sprint2/storebackend/StoreBackendApplication.java
@@ -2,6 +2,11 @@ package com.sprint2.storebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import com.sprint2.storebackend.model.Product;
+import com.sprint2.storebackend.service.ProductService;
 
 @SpringBootApplication
 public class StoreBackendApplication {
@@ -9,5 +14,18 @@ public class StoreBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StoreBackendApplication.class, args);
 	}
+	
+	@Bean
+    CommandLineRunner runner(ProductService productService) {
+        return args -> {
+            productService.save(new Product(1L, "TV Set", 300.00, "http://placehold.it/200x100"));
+            productService.save(new Product(2L, "Game Console", 200.00, "http://placehold.it/200x100"));
+            productService.save(new Product(3L, "Sofa", 100.00, "http://placehold.it/200x100"));
+            productService.save(new Product(4L, "Icecream", 5.00, "http://placehold.it/200x100"));
+            productService.save(new Product(5L, "Beer", 3.00, "http://placehold.it/200x100"));
+            productService.save(new Product(6L, "Phone", 500.00, "http://placehold.it/200x100"));
+            productService.save(new Product(7L, "Watch", 30.00, "http://placehold.it/200x100"));
+        };
+    }
 
 }
